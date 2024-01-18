35a98501b79409cd9fcc9fccdbdf70e78a36444b
4ce <88108711+4cecoder@users.noreply.github.com>
2024-01-18 13:21:17 -0500

    Update main.go

--- client/main.go
+++ client/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	fmt.Println("Server starting on :8081...")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
